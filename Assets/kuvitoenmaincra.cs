086c17c3cb6350ab8398ee5920dfbd3293dea19b
JP <dominguez.juanp30@gmail.com>
2025-04-16 00:34:45 -0500

    Initial commit

---
+++ Assets/kuvitoenmaincra.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class Kuvitoenmaincra : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+
+    private void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
