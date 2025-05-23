ee201ab09393701fa52f7c5100f20c65477c82b6
miguel  angel reyes mendoza <miguemendoza1@gmail.com>
2025-05-23 02:02:20 -0500

    Following Camera

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-namespace Player
+namespace Camera
 {
     public class CameraMovement : MonoBehaviour
     {
@@ -10,17 +10,49 @@ namespace Player
         [SerializeField] private float cameraSpeed = 0.1f;
 
         [SerializeField] private Vector3 offset;
+        [SerializeField] private float cameraAreaPosition = 3f;
+        [SerializeField] private float cameraLimitArea = 0.2f;
 
         private void Start()
         {
             Application.targetFrameRate = 60;
         }
-
+        
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(playerTransform.position, 1);
+            Gizmos.DrawWireSphere(transform.position, cameraAreaPosition);
+        }
+        
         private void LateUpdate()
         {
+
+            float xPosition = 0f;
+            float yPosition = 0f;
+            if (playerTransform.position.x <= transform.position.x + cameraAreaPosition + cameraLimitArea &&
+                 playerTransform.position.x >= transform.position.x + cameraAreaPosition - cameraLimitArea)
+            {
+                xPosition += cameraLimitArea;
+            }
+            else if (playerTransform.position.x <= transform.position.x - cameraAreaPosition + cameraLimitArea &&
+                playerTransform.position.x >= transform.position.x - cameraAreaPosition - cameraLimitArea)
+            {
+                xPosition -= cameraLimitArea;
+            }
+            if (playerTransform.position.y <= transform.position.y + cameraAreaPosition + cameraLimitArea &&
+                playerTransform.position.y >= transform.position.y + cameraAreaPosition - cameraLimitArea)
+            {
+                yPosition += cameraLimitArea;
+            }
+            else if (playerTransform.position.y <= transform.position.y - cameraAreaPosition + cameraLimitArea &&
+                playerTransform.position.y >= transform.position.y - cameraAreaPosition - cameraLimitArea)
+            {
+                yPosition -= cameraLimitArea;
+            }
             transform.position = new Vector3(
-                playerTransform.position.x + offset.x,
-                playerTransform.position.y + offset.y,
+                transform.position.x + xPosition,
+                transform.position.y + yPosition,
                 transform.position.z  
             );
         }
